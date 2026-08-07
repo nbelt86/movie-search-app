0ebc448c0ea1a29a10bcd8e957e2b46fe87cb54b
Nathan Beltran nbelt86@gmail.com <nbelt86@gmail.com>
2026-08-07 16:56:48 +0900

    added event listeners and search functions.

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 // This is yours to build, one line at a time.
+const apiKey = "e105a334"
+const searchInput = document.querySelector("#search-input")
+const searchBtn = document.querySelector("#search-btn")
+
+searchBtn.addEventListener("click", async () => {
+  const searchTerm = searchInput.value;
+  const url = `https://www.omdbapi.com/?apikey=${apiKey}&s=${searchTerm}`;
+  const response = await fetch(url);
+
+});
